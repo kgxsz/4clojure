6b9591e4cc508a8a1c8de64f549fbb22175a4432
Keigo Suzukawa <k.suzukawa@gmail.com>
2014-10-15 14:18:47 +1100

    Add merge-with-function problem

--- medium.clj
+++ medium.clj
@@ -308,3 +308,25 @@
 (= (take 5 (sequence-reductions + (range))) [0 1 3 6 10])
 (= (sequence-reductions conj [1] [2 3 4]) [[1] [1 2] [1 2 3] [1 2 3 4]])
 (= (last (sequence-reductions * 2 [3 4 5])) (reduce * 2 [3 4 5]) 120)
+
+
+; merge-with-a-function
+
+; Write a function which takes a function f and a variable number of maps. Your function should return a map that
+; consists of the rest of the maps conj-ed onto the first. If a key occurs in more than one map, the mapping(s)
+; from the latter (left-to-right) should be combined with the mapping in the result by calling (f val-in-result val-in-latter)
+; Special Restrictions - merge-with
+
+(defn merge-with-a-function [f & xs]
+  (let [ys (group-by first (mapcat seq xs))]
+    (->> (vals ys)
+         (map #(map second %))
+         (map #(reduce f %))
+         (zipmap (keys ys)))))
+
+(= (merge-with-a-function * {:a 2, :b 3, :c 4} {:a 2} {:b 2} {:c 5})
+   {:a 4, :b 6, :c 20})
+(= (merge-with-a-function - {1 10, 2 20} {1 3, 2 10, 3 15})
+   {1 7, 2 10, 3 15})
+(= (merge-with-a-function concat {:a [3], :b [6]} {:a [4 5], :c [8 9]} {:b [7]})
+   {:a [3 4 5], :b [6 7], :c [8 9]})

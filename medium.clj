d8b6b86e509f6f1c03800b2c048a7d45cb304ca0
Keigo Suzukawa <k.suzukawa@gmail.com>
2015-03-12 21:18:08 +0000

    Add palindromic numbers problem

--- medium.clj
+++ medium.clj
@@ -494,3 +494,43 @@
 (= {:a [1], :b [2]} (identify-keys-and-values [:a 1, :b 2]))
 (= {:a [1 2 3], :b [], :c [4]} (identify-keys-and-values [:a 1 2 3 :b :c 4]))
 
+
+;; palindromic-numbers
+
+;; A palindromic number is a number that is the same when written forwards or backwards  (e.g., 3, 99, 14341).
+;; Write a function which takes an integer n, as its only argument, and returns an increasing lazy sequence of
+;; all palindromic numbers that are not less than n. The most simple solution will exceed the time limit!
+
+(defn palindromic-numbers [n]
+  (letfn [(palindrome? [xs] (= (-> xs str seq) (-> xs str seq reverse)))
+          (up-range [n] (map #(+ n %) (range)))]
+  (filter palindrome? (up-range n))))
+
+;; work in progress
+
+(=  (take 26  (palindromic-numbers 0))
+      [0 1 2 3 4 5 6 7 8 9
+           11 22 33 44 55 66 77 88 99
+               101 111 121 131 141 151 161])
+
+(=  (take 16  (palindromic-numbers 162))
+      [171 181 191 202
+           212 222 232 242
+               252 262 272 282
+                   292 303 313 323])
+
+(=  (take 6  (palindromic-numbers 1234550000))
+      [1234554321 1234664321 1234774321
+           1234884321 1234994321 1235005321])
+
+(=  (first  (palindromic-numbers  (* 111111111 111111111)))
+      (* 111111111 111111111))
+
+(=  (set  (take 199  (palindromic-numbers 0)))
+      (set  (map #(first  (palindromic-numbers %))  (range 0 10000))))
+
+(= true
+      (apply <  (take 6666  (palindromic-numbers 9999999))))
+
+(=  (nth  (palindromic-numbers 0) 10101)
+      9102019)

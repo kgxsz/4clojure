764dc205c9389558ceb3cefcdd09875a10d56590
Keigo Suzukawa <k.suzukawa@gmail.com>
2015-08-19 08:47:54 +0100

    Transfer all the medium problems

--- src/problems.clj
+++ src/problems.clj
@@ -16,7 +16,6 @@
 (defn comparisons [less-than x y]
   (if (less-than x y) :lt (if (less-than y x) :gt :eq)))
 
-
 (defn greatest-common-divisor [x y]
   (->> (range (min x y))
        (map inc)
@@ -165,7 +164,7 @@
 (defn group-a-sequence [f xs]
   (group-by f xs)
   #_(->> (map hash-map (map f xs) xs)
-       (apply merge-with vector)))
+         (apply merge-with vector)))
 
 (defn read-a-binary-number [xs]
   (->> (reverse (seq xs))
@@ -236,3 +235,186 @@
 
 (defn pascals-trapezoid [xs]
   (iterate #(vec (map +' (concat [0] %) (concat % [0]))) xs))
+
+(defn prime-numbers [n]
+  (->> (range)
+       (drop 2)
+       (filter (fn [x] (every? #(pos? (rem x %)) (range 2 x))))
+       (take n)))
+
+(defn rotate-a-sequence [n xs]
+  (let [r (mod n (count xs))]
+    (concat (drop r xs) (take r xs))))
+
+(defn juxtaposition [& fs]
+  (fn [& xs]
+    (for [f fs]
+      (apply f xs))))
+
+(defn filter-perfect-squares [s]
+  (->>
+    (clojure.string/split s #",")
+    (map #(Integer/parseInt %))
+    (filter #(= 0.0 (mod (Math/sqrt %) 1)))
+    (interpose ",")
+    (apply str)))
+
+(defn perfect-numbers [x]
+  (->>
+    (range 1 x)
+    (filter #(zero? (mod x %)))
+    (reduce +)
+    (= x)))
+
+(defn flipping-out [f]
+  (fn [x y]
+    (f y x)))
+
+(defn reverse-interleave [xs n]
+  (apply map vector (partition n xs)))
+
+(defn split-by-type [xs]
+  (vals (group-by type xs)))
+
+(defn count-occurences [xs]
+  (let [m (group-by identity xs)]
+    (zipmap (keys m) (map count (vals m)))))
+
+(defn find-distinct-items [xs]
+  (->>
+    (group-by identity xs)
+    (keys)
+    (sort-by #(.indexOf xs %))))
+
+(defn function-composition [& fs]
+  (fn [& xs]
+    (first (reduce #(vector (apply %2 %1)) xs (reverse fs)))))
+
+(defn partition-a-sequence [x xs]
+  (for [k (range (quot (count xs) x))
+        :let [n (* k x)]]
+    (take x (drop n xs))))
+
+(defn word-sorting [s]
+  (sort-by #(clojure.string/lower-case %) (re-seq #"\w+" s)))
+
+(defn black-box-testing [xs]
+  (let [x (flatten (seq (conj xs [:x :x] [:x :x] [:x :y])))
+        nx (count x)
+        y (flatten (seq xs))
+        ny (count y)]
+    (cond
+      (= (- nx 2) ny) :map
+      (= (- nx 4) ny) :set
+      (not= (last x) :y) :list
+      (= (last x) :y) :vector)))
+
+(def intro-to-trampoline
+  (letfn
+    [(foo [x y] #(bar (conj x y) y))
+     (bar [x y] (if (> (last x) 10)
+                  x
+                  #(foo x (+ 2 y))))]
+    (trampoline foo [] 1)))
+
+(defn anagram-finder [xs]
+  (->> (vals (group-by sort xs))
+       (filter #(> (count %) 1))
+       (map set)
+       (set)))
+
+(defn sequence-reductions
+  ([f xs]
+   (sequence-reductions f (first xs) (rest xs)))
+  ([f x xs]
+   (lazy-seq
+     (cons x
+           (if (empty? xs) [] (sequence-reductions f (f x (first xs)) (rest xs)))))))
+
+(defn merge-with-a-function [f & xs]
+  (let [ys (group-by first (mapcat seq xs))]
+    (->> (vals ys)
+         (map #(map second %))
+         (map #(reduce f %))
+         (zipmap (keys ys)))))
+
+(defn into-camel-case [xs]
+  (let [split-xs (clojure.string/split xs #"-")]
+    (->> (rest split-xs)
+         (map clojure.string/capitalize)
+         (cons (first split-xs))
+         (apply str))))
+
+(defn happy-numbers [x]
+  (letfn [(happy [xs]
+            (apply + (for [i (str xs)]
+                       (#(* % %) (Integer/parseInt (str i))))))
+          (happy? [s x]
+            (cond
+              (= x 1) true
+              (contains? s x) false
+              :else (recur (conj s x) (happy x))))]
+    (happy? #{} x)))
+
+(defn eulers-totient-function [x]
+  (letfn [(gcd [a b] (if (zero? b) a (recur b (mod a b))))]
+    (->> (map #(gcd % x) (range 1 (inc x)))
+      (filter #{1})
+      (count))))
+
+(defn power-set [xs]
+  (let [n (count xs)
+        range-combinations (range (Math/pow 2 n))
+        decimal->binary (partial clojure.pprint/cl-format nil "~v,'0B" n)]
+    (set (for [y (map decimal->binary range-combinations)]
+           (->> (map vector y xs)
+                (filter #(= \1 (first %)))
+                (map #(second %))
+                (set))))))
+
+(defn the-balance-of-n [n]
+  (let [seq-n (map int (str n))
+        halve #(int (/ % 2))
+        len (count seq-n)
+        half-len (halve len)]
+    (->> [(take half-len seq-n) (take-last half-len seq-n)]
+         (map #(reduce + %))
+         (apply =))))
+
+(defn digits-and-bases [n b]
+  (if (< n b) [n] (conj (digits-and-bases (quot n b) b) (mod n b))))
+
+(defn identify-keys-and-values [[x & xs]]
+  (if x
+    (assoc
+      (identify-keys-and-values (drop-while number? xs))
+      x
+      (take-while number? xs))
+    {}))
+
+(defn palindromic-numbers [k]
+  (let [sequify (fn [n] (->> n str seq (map str) (map read-string)))
+        indices (fn [n] (-> n sequify count (/ 2) int range))
+        mirrorize (fn [n i]
+                    (let [seq-n (sequify n)
+                          a (nth seq-n i)
+                          b (nth (reverse seq-n) i)
+                          factor (reduce * 1 (repeat i 10))
+                          offset (mod (- a b) 10)]
+                      (+ n (* offset factor))))
+        palindromize (fn [n] (reduce #(mirrorize %1 %2) n (indices n)))
+        next-palindrome (fn [n] (-> (palindromize (inc n)) (palindromize)))]
+    (rest (iterate next-palindrome (dec k)))))
+
+(defn balanced-primes [n]
+  (let [mean #(/ (+ %1 %2) 2)
+        prime? (fn [n]
+                 (and
+                   (> n 1)
+                   (->> (range 2 n)
+                        (map #(mod n %))
+                        (not-any? zero?))))
+        first-prime #(first (filter prime? %))
+        next-prime (fn [n] ( first-prime (map #(+ % n 1) (range))))
+        prev-prime #(first-prime (range (dec %) 0 -1))]
+    (and (prime? n) (> n 2) (= n (mean (prev-prime n) (next-prime n))))))
--- test/problems_test.clj
+++ test/problems_test.clj
@@ -364,3 +364,179 @@
   (is (= (take 5 (pascals-trapezoid [1])) [[1] [1 1] [1 2 1] [1 3 3 1] [1 4 6 4 1]]))
   (is (= (take 2 (pascals-trapezoid [3 1 2])) [[3 1 2] [3 4 3 2]]))
   (is (= (take 100 (pascals-trapezoid [2 4 2])) (rest (take 101 (pascals-trapezoid [2 2]))))))
+
+(deftest test-prime-numbers
+  (is (= (prime-numbers 2) [2 3]))
+  (is (= (prime-numbers 5) [2 3 5 7 11]))
+  (is (= (last (prime-numbers 100)) 541)))
+
+(deftest test-rotate-a-sequence
+  (is (= (rotate-a-sequence 2 [1 2 3 4 5]) '(3 4 5 1 2)))
+  (is (= (rotate-a-sequence -2 [1 2 3 4 5]) '(4 5 1 2 3)))
+  (is (= (rotate-a-sequence 6 [1 2 3 4 5]) '(2 3 4 5 1)))
+  (is (= (rotate-a-sequence 1 '(:a :b :c)) '(:b :c :a)))
+  (is (= (rotate-a-sequence -4 '(:a :b :c)) '(:c :a :b))))
+
+(deftest test-juxtaposition
+  (is (= [21 6 1] ((juxtaposition + max min) 2 3 5 1 6 4)))
+  (is (= ["HELLO" 5] ((juxtaposition #(.toUpperCase %) count) "hello")))
+  (is (= [2 6 4] ((juxtaposition :a :c :b) {:a 2, :b 4, :c 6, :d 8 :e 10}))))
+
+(deftest test-filter-perfect-squares
+  (is (= (filter-perfect-squares "4,5,6,7,8,9") "4,9"))
+  (is (= (filter-perfect-squares "15,16,25,36,37") "16,25,36")))
+
+(deftest test-perfect-numbers
+  (is (= (perfect-numbers 6) true))
+  (is (= (perfect-numbers 7) false))
+  (is (= (perfect-numbers 496) true))
+  (is (= (perfect-numbers 500) false))
+  (is (= (perfect-numbers 8128) true)))
+
+(deftest test-flipping-out
+  (is (= 3 ((flipping-out nth) 2 [1 2 3 4 5])))
+  (is (= true ((flipping-out >) 7 8)))
+  (is (= 4 ((flipping-out quot) 2 8)))
+  (is (= [1 2 3] ((flipping-out take) [1 2 3 4 5] 3))))
+
+(deftest test-reverse-interleave
+  (is (= (reverse-interleave [1 2 3 4 5 6] 2) '((1 3 5) (2 4 6))))
+  (is (= (reverse-interleave (range 9) 3) '((0 3 6) (1 4 7) (2 5 8))))
+  (is (= (reverse-interleave (range 10) 5) '((0 5) (1 6) (2 7) (3 8) (4 9)))))
+
+(deftest test-split-by-type
+  (is (= (set (split-by-type [1 :a 2 :b 3 :c])) #{[1 2 3] [:a :b :c]}))
+  (is (= (set (split-by-type [:a "foo"  "bar" :b])) #{[:a :b] ["foo" "bar"]}))
+  (is (= (set (split-by-type [[1 2] :a [3 4] 5 6 :b])) #{[[1 2] [3 4]] [:a :b] [5 6]})))
+
+(deftest test-count-occurences
+  (is (= (count-occurences [1 1 2 3 2 1 1]) {1 4, 2 2, 3 1}))
+  (is (= (count-occurences [:b :a :b :a :b]) {:a 2, :b 3}))
+  (is (= (count-occurences '([1 2] [1 3] [1 3])) {[1 2] 1, [1 3] 2})))
+
+(deftest test-find-distinct-items
+  (is (= (find-distinct-items [1 2 1 3 1 2 4]) [1 2 3 4]))
+  (is (= (find-distinct-items [:a :a :b :b :c :c]) [:a :b :c]))
+  (is (= (find-distinct-items '([2 4] [1 2] [1 3] [1 3])) '([2 4] [1 2] [1 3])))
+  (is (= (find-distinct-items (range 50)) (range 50))))
+
+(deftest test-function-composition
+  (is (= [3 2 1] ((function-composition rest reverse) [1 2 3 4])))
+  (is (= 5 ((function-composition (partial + 3) second) [1 2 3 4])))
+  (is (= true ((function-composition zero? #(mod % 8) +) 3 5 7 9)))
+  (is (= "HELLO" ((function-composition #(.toUpperCase %) #(apply str %) take) 5 "hello world"))))
+
+(deftest test-partition-a-sequence
+  (is (= (partition-a-sequence 3 (range 9)) '((0 1 2) (3 4 5) (6 7 8))))
+  (is (= (partition-a-sequence 2 (range 8)) '((0 1) (2 3) (4 5) (6 7))))
+  (is (= (partition-a-sequence 3 (range 8)) '((0 1 2) (3 4 5)))))
+
+(deftest test-word-sorting
+  (is (= (word-sorting  "Have a nice day.")
+         ["a" "day" "Have" "nice"]))
+  (is (= (word-sorting  "Clojure is a fun language!")
+         ["a" "Clojure" "fun" "is" "language"]))
+  (is (= (word-sorting  "Fools fall for foolish follies.")
+         ["fall" "follies" "foolish" "Fools" "for"])))
+
+(deftest test-black-box-testing
+  (is (= :map (black-box-testing {:a 1, :b 2})))
+  (is (= :list (black-box-testing (range (rand-int 20)))))
+  (is (= :vector (black-box-testing [1 2 3 4 5 6])))
+  (is (= :set (black-box-testing #{10 (rand-int 5)})))
+  (is (= [:map :set :vector :list] (map black-box-testing [{} #{} [] ()]))))
+
+(deftest test-intro-to-trampoline
+  (= [1 3 5 7 9 11] intro-to-trampoline))
+
+(deftest test-anagram-finder
+  (is (= (anagram-finder ["meat" "mat" "team" "mate" "eat"])
+         #{#{"meat" "team" "mate"}}))
+  (is (= (anagram-finder ["veer" "lake" "item" "kale" "mite" "ever"])
+         #{#{"veer" "ever"} #{"lake" "kale"} #{"mite" "item"}})))
+
+(deftest test-sequence-reductions
+  (is (= (take 5 (sequence-reductions + (range))) [0 1 3 6 10]))
+  (is (= (sequence-reductions conj [1] [2 3 4]) [[1] [1 2] [1 2 3] [1 2 3 4]]))
+  (is (= (last (sequence-reductions * 2 [3 4 5])) (reduce * 2 [3 4 5]) 120)))
+
+(deftest test-merge-with-a-function
+  (is (= (merge-with-a-function * {:a 2, :b 3, :c 4} {:a 2} {:b 2} {:c 5})
+         {:a 4, :b 6, :c 20}))
+  (is (= (merge-with-a-function - {1 10, 2 20} {1 3, 2 10, 3 15})
+         {1 7, 2 10, 3 15}))
+  (is (= (merge-with-a-function concat {:a [3], :b [6]} {:a [4 5], :c [8 9]} {:b [7]})
+         {:a [3 4 5], :b [6 7], :c [8 9]})))
+
+(deftest test-into-camel-case
+  (is (= (into-camel-case "something") "something"))
+  (is (= (into-camel-case "multi-word-key") "multiWordKey"))
+  (is (= (into-camel-case "leaveMeAlone") "leaveMeAlone")))
+
+(deftest test-happy-numbers
+  (is (= (happy-numbers 7) true))
+  (is (= (happy-numbers 986543210) true))
+  (is (= (happy-numbers 2) false))
+  (is (= (happy-numbers 3) false)))
+
+(deftest test-eulers-totient-function
+  (is (= (eulers-totient-function 1) 1))
+  (is (= (eulers-totient-function 10) (count '(1 3 7 9)) 4))
+  (is (= (eulers-totient-function 40) 16))
+  (is (= (eulers-totient-function 99) 60)))
+
+(deftest test-power-set
+  (is (= (power-set #{1 :a}) #{#{1 :a} #{:a} #{} #{1}}))
+  (is (= (power-set #{}) #{#{}}))
+  (is (= (power-set #{1 2 3})
+         #{#{} #{1} #{2} #{3} #{1 2} #{1 3} #{2 3} #{1 2 3}}))
+  (is (= (count (power-set (into #{} (range 10)))) 1024)))
+
+(deftest test-the-balance-of-n
+  (is (= true (the-balance-of-n 11)))
+  (is (= true (the-balance-of-n 121)))
+  (is (= false (the-balance-of-n 123)))
+  (is (= true (the-balance-of-n 0)))
+  (is (= false (the-balance-of-n 88099)))
+  (is (= true (the-balance-of-n 89098)))
+  (is (= true (the-balance-of-n 89089)))
+  (is (= (take 20 (filter the-balance-of-n (range)))
+         [0 1 2 3 4 5 6 7 8 9 11 22 33 44 55 66 77 88 99 101])))
+
+(deftest test-digits-and-bases
+  (is (= [1 2 3 4 5 0 1] (digits-and-bases 1234501 10)))
+  (is (= [0] (digits-and-bases 0 11)))
+  (is (= [1 0 0 1] (digits-and-bases 9 2)))
+  (is (= [1 0] (let [n (rand-int 100000)](digits-and-bases n n))))
+  (is (= [16 18 5 24 15 1] (digits-and-bases Integer/MAX_VALUE 42))))
+
+(deftest test-identify-keys-and-values
+  (is (= {} (identify-keys-and-values [])))
+  (is (= {:a [1]} (identify-keys-and-values [:a 1])))
+  (is (= {:a [1], :b [2]} (identify-keys-and-values [:a 1, :b 2])))
+  (is (= {:a [1 2 3], :b [], :c [4]} (identify-keys-and-values [:a 1 2 3 :b :c 4]))))
+
+(deftest test-palindromic-numbers
+  (is (=  (take 26  (palindromic-numbers 0))
+         [0 1 2 3 4 5 6 7 8 9
+          11 22 33 44 55 66 77 88 99
+          101 111 121 131 141 151 161]))
+  (is (=  (take 16  (palindromic-numbers 162))
+         [171 181 191 202
+          212 222 232 242
+          252 262 272 282
+          292 303 313 323]))
+  (is (=  (take 6  (palindromic-numbers 1234550000))
+         [1234554321 1234664321 1234774321
+          1234884321 1234994321 1235005321]))
+  (is (=  (first  (palindromic-numbers  (* 111111111 111111111)))
+         (* 111111111 111111111)))
+  (is (=  (set  (take 199  (palindromic-numbers 0)))
+         (set  (map #(first  (palindromic-numbers %))  (range 0 10000)))))
+  (is (= true (apply <  (take 6666  (palindromic-numbers 9999999)))))
+  (is (=  (nth  (palindromic-numbers 0) 10101) 9102019)))
+
+(deftest test-balanced-primes
+  (is (= false  (balanced-primes 4)))
+  (is (= true  (balanced-primes 563)))
+  (is (= 1103  (nth  (filter balanced-primes (range)) 15))))
